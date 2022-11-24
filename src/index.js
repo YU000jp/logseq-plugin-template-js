b79994fee0c73245c4788efb65afb27ad6f4f69e
YU <111847207+YU000jp@users.noreply.github.com>
2022-11-24 11:06:59 +0900

    query & json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import '@logseq/libs';
 import { getDateForPage } from 'logseq-dateutils';
+import { json } from 'stream/consumers';
 import { logseq as PL } from "../package.json";
 
 
@@ -22,20 +23,20 @@ const main = () => {
   `);
 
   /* ContextMenuItem  */
-    logseq.Editor.registerBlockContextMenuItem('🟢Open at right sidebar', async ({uuid}) => {
- 0
-      /* todayDateInUserFormat Sample */
-      const userConfigs = await logseq.App.getUserConfigs();
-      const preferredDateFormat = userConfigs.preferredDateFormat;
-      const today = new Date();
-      const todayDateInUserFormat = getDateForPage(today, preferredDateFormat);
-      console.log(`#${pluginId}: ${todayDateInUserFormat}`);
-      /* end_sample */
+  logseq.Editor.registerBlockContextMenuItem('🟢Open at right sidebar', async ({ uuid }) => {
 
-      logseq.App.openInRightSidebar(uuid);
-      logseq.UI.showMsg("🟢Open at right sidebar");
-      console.log(`#${pluginId}: 🟢Open at right sidebar`);
-    });
+    /* todayDateInUserFormat Sample */
+    const userConfigs = await logseq.App.getUserConfigs();
+    const preferredDateFormat = userConfigs.preferredDateFormat;
+    const today = new Date();
+    const todayDateInUserFormat = getDateForPage(today, preferredDateFormat);
+    console.log(`#${pluginId}: ${todayDateInUserFormat}`);
+    /* end_sample */
+
+    logseq.App.openInRightSidebar(uuid);
+    logseq.UI.showMsg("🟢Open at right sidebar");
+    console.log(`#${pluginId}: 🟢Open at right sidebar`);
+  });
 
   /* toolbarItem */
   logseq.App.registerUIItem("toolbar", {
@@ -44,7 +45,7 @@ const main = () => {
     <div data-on-click="open_dashboard" style="font-size:20px">🔥</div>
     `,
   });/* For open_dashboard */
-  
+
   console.info(`#${pluginId}: loaded`);
 };/* end_main */
 
@@ -53,11 +54,30 @@ const main = () => {
 const model = {
   async open_dashboard() {
     console.info(`#${pluginId}: open_dashboard`);
+    logseq.UI.showMsg(`Open dashboard`);
+
     const queryScript = logseq.settings.advancedQuery;
     console.log(`#${pluginId}: queryScript ${queryScript}`); /* TODO */
-    const result = await logseq.DB.datascriptQuery(queryScript);
-      console.log(`#${pluginId}: result ${result}`);
-    logseq.UI.showMsg(`Open dashboard`);
+    const queryResult = await logseq.DB.datascriptQuery(queryScript);
+      console.log(`#${pluginId}: Advanced Query result`);
+      console.log(queryResult);
+    logseq.UI.showMsg(`Advanced Query`);
+
+    /* JSON */
+    const settingJsonUrl = logseq.settings.jsonUrl;
+    if(settingJsonUrl != ""){
+    const jsonFetch = async (url) => {
+      const response = await fetch(url); //await で fetch() が完了するまで待つ
+      const jsonData = await response.json(); //await で response.json() が完了するまで待つ
+      console.log(`#${pluginId}: JSON result`);
+      console.log(jsonData);
+      logseq.UI.showMsg(`Fetch JSON`);
+    }
+    jsonFetch(settingJsonUrl);
+  }else{
+    logseq.UI.showMsg(`プラグインの設定をおこなってください。`);
+  } 
+
   }
 };
 
--- src/setting.js
+++ src/setting.js
@@ -1,17 +1,24 @@
 export const settingUI = () => {
-/* https://logseq.github.io/plugins/types/SettingSchemaDesc.html */
+    /* https://logseq.github.io/plugins/types/SettingSchemaDesc.html */
     const settingsTemplate = [
         {
             key: "advancedQuery",
             type: "string",
             inputAs: 'textarea',
-            default: 
+            default:
 `[:find (pull ?e [*]) 
 :where
 [?e :block/marker ?m]
 [(contains? #{"DOING"} ?m)]]`,
             title: "Customize advanced-query",
             description: ``,
+        },
+        {
+            key: "jsonUrl",
+            type: "string",
+            default: ``,
+            title: "Converted .csv to .json",
+            description: `変換用サイトで、コピーしたURLを貼り付けてください。`,
         }
     ];
     logseq.useSettingsSchema(settingsTemplate);
